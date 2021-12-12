08196a060000bea3ea2a430fefc4586b5390f63a
soottoppi <duwjs1103@naver.com>
2021-12-12 14:51:13 +0900

    1212 14:51 invite 모달 수정중.. by soo

--- src/sidebar/User.js
+++ src/sidebar/User.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import UserImg from '../assets/images/user11.png'
 
-const User = ({no, name, image, deptUserDatas}) => {
-
+const User = ({no, name, image, isInvited}) => {
 
     const handleChange = (e) => {
-        
+        e.target.id // user의 no
     }
 
     return (
         <div>
             <img src={UserImg} alt="" />
             <label >{name}</label>
-            <input onChange={handleChange} id={no} type='checkbox'></input>
+            <input disabled={isInvited} onChange={handleChange} id={no} type='checkbox'></input>
         </div>
     );
 };
